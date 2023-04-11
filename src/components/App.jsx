15fd104ee2afff7a48c34d10c185de836b23a8d9
marinamelnik1105 <masha.nasha119@gmail.com>
2023-04-11 16:33:01 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,47 +4,6 @@ import { Filter } from './Filter/Filter';
 import { GlobalStyle } from './GlobalStyle.styled';
 import { Layout } from './Layout.styled';
 export const App = () => {
-  // const [contacts, setContacts] = useState(() => {
-  //   const savedContacts = localStorage.getItem('contacts');
-  //   if (savedContacts !== null) {
-  //     const parsedContacts = JSON.parse(savedContacts);
-  //     return parsedContacts;
-  //   }
-  //   return initalContacts;
-  // });
-
-  // const [filter, setFilter] = useState('');
-
-  // useEffect(() => {
-  //   localStorage.setItem('contacts', JSON.stringify(contacts));
-  // }, [contacts]);
-  // const addContacts = newContacts => {
-  //   const repeatCheck = () => {
-  //     return contacts.some(value => {
-  //       return value.name.toLowerCase() === newContacts.name.toLowerCase();
-  //     });
-  //   };
-  //   if (repeatCheck()) {
-  //     return alert(`${newContacts.name} is already in contacts.`);
-  //   } else {
-  //     setContacts(prevState => [...prevState, newContacts]);
-  //   }
-  // };
-  // const deleteContacts = contactId => {
-  //   setContacts(prevState =>
-  //     prevState.filter(contact => contact.id !== contactId)
-  //   );
-  // };
-  // const changeFilter = e => {
-  //   setFilter(e.target.value);
-
-  // const getVisibleContacts = () => {
-  //   const normalizedFilter = filter.toLowerCase();
-  //   return contacts.filter(contact =>
-  //     contact.name.toLowerCase().includes(normalizedFilter)
-  //   );
-
-  // const visibleContacts = getVisibleContacts();
   return (
     <Layout>
       <GlobalStyle />
